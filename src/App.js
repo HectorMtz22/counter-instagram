bc82ebccae28a1cc04e66e541cdc8881749077da
HectorMtz22 <hec.gre81@gmail.com>
2021-07-17 15:20:40 -0500

    Logic for counters

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
+import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [dev, setDev] = useState(0)
+  const [music, setMusic] = useState(0)
+  const [nose, setNose] = useState(0)
+
+  const handleKey = (e) => {
+    if (e.key === 'j') {
+      setDev(dev + 1)
+    } else if(e.key === 'k') {
+      setMusic(music + 1)
+    } else {
+      setNose(nose + 1)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Developers: {dev}</p>
+        <p>Musicians: {music}</p>
+        <p>I don't Know: {nose}</p>
+        <input type="text" onKeyPress={(e) => handleKey(e)} autoFocus/>
+        
+        
       </header>
     </div>
   );
